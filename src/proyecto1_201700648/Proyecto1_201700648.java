720e566d22ebe1fc136096d7ee285920565d7cfd
Marthinn Chew <Marthinn Chew@DESKTOP-J6VSV4B>
2018-09-06 18:01:30 -0600

    Pantalla de Bienvenida

---
+++ src/proyecto1_201700648/Proyecto1_201700648.java
@@ -0,0 +1,21 @@
+
+package proyecto1_201700648;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+/**
+ *
+ * @author Marthinn Chew
+ */
+public class Proyecto1_201700648 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Bienvenida llamada = new Bienvenida();
+       llamada.Ventana();//llamo a la ventana
+    }
+    
+}
